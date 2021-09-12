cf7b6170171c86fb9d182980e688f165d25a2f37
da-moon <damoon.azarpazhooh@live.com>
2021-09-12 18:28:24 -0400

    minor fixes to installer script

--- spacevim-installer.sh
+++ spacevim-installer.sh
@@ -30,9 +30,10 @@ sudo yarn --silent global add --prefix /usr/local \
   dockerfile-language-server-nodejs \
   standard-readme-spec
 python3 -m pip install --user notedown
-/bin/rm -rf \
+sudo /bin/rm -rf \
     ~/.SpaceVim \
     ~/.vim* \
+    ~/.config/coc \
     ~/.config/*vim* \
     ~/.cache/*vim* \
     ~/.cache/SpaceVim \
@@ -42,32 +43,31 @@ python3 -m pip install --user notedown
 && sed -i.bak 's/call dein#add/"call dein#add/g' "$HOME/.SpaceVim/autoload/SpaceVim/plugins.vim" \
 && mkdir -p "$HOME/.local/share/nvim/shada"
 # [ NOTE ] => https://herringtondarkholme.github.io/2016/02/26/dein/
-#  :UpdateRemotePlugins |
 nvim --headless \
-  -c ":echon \"\\n\\n######\\tInitial Setup ...\\t#####\\n\\n\"" \
-  -c "call dein#direct_install('deoplete-plugins/deoplete-go', { 'build': 'make','hook_post_update': ':UpdateRemotePlugins'})" \
-  -c "call dein#direct_install('Shougo/vimproc.vim', { 'build': 'make' })" \
-  -c "call dein#direct_install('iamcco/markdown-preview.nvim', {'on_ft': ['markdown', 'pandoc.markdown', 'rmd'],'build': 'yarn --cwd app --frozen-lockfile install' })" \
-  -c "call dein#direct_install('lymslive/vimloo', { 'merged': '0' })" \
-  -c "call dein#direct_install('lymslive/vnote', { 'depends': 'vimloo' })" \
-  -c "qall"
+ -c ":echon \"\\n\\n######\\tInitial Setup ...\\t#####\\n\\n\"" \
+ -c "call dein#direct_install('deoplete-plugins/deoplete-go', { 'build': 'make','hook_post_update': ':UpdateRemotePlugins'})" \
+ -c "call dein#direct_install('Shougo/vimproc.vim', { 'build': 'make' })" \
+ -c "call dein#direct_install('iamcco/markdown-preview.nvim', {'on_ft': ['markdown', 'pandoc.markdown', 'rmd'],'build': 'yarn --cwd app --frozen-lockfile install' })" \
+ -c "call dein#direct_install('lymslive/vimloo', { 'merged': '0' })" \
+ -c "call dein#direct_install('lymslive/vnote', { 'depends': 'vimloo' })" \
+ -c "qall"
 if [ -r "$HOME/.cache/vimfiles/repos/github.com/zchee/deoplete-go/rplugin/python3/deoplete/sources/deoplete_go.py" ]; then
 sed -i \
-    -e '/def gather_candidates/a\        return []' \
-    -e '/def get_complete_result/a\        return []' \
-    -e '/def find_gocode_binary/a\        return "/bin/true"' \
+   -e '/def gather_candidates/a\        return []' \
+   -e '/def get_complete_result/a\        return []' \
+   -e '/def find_gocode_binary/a\        return "/bin/true"' \
 "$HOME/.cache/vimfiles/repos/github.com/zchee/deoplete-go/rplugin/python3/deoplete/sources/deoplete_go.py"
 echo 'finish' >  "$HOME/.cache/vimfiles/repos/github.com/zchee/deoplete-go/plugin/deoplete-go.vim"
 fi
 mv "$HOME/.SpaceVim/autoload/SpaceVim/plugins.vim.bak" "$HOME/.SpaceVim/autoload/SpaceVim/plugins.vim"
 # https://raw.githubusercontent.com/arthurnavah/environment/master/update.sh
 nvim --headless \
-  -c "call dein#clear_state()" \
-  -c "call dein#install()" \
-  -c "call dein#update()" \
-  -c "call dein#recache_runtimepath()" \
-  -c "call dein#remote_plugins()" \
-  -c "UpdateRemotePlugins" \
-  -c "call dein#save_state()" \
-  -c "qall"
+ -c "call dein#clear_state()" \
+ -c "call dein#install()" \
+ -c "call dein#update()" \
+ -c "call dein#recache_runtimepath()" \
+ -c "call dein#remote_plugins()" \
+ -c "UpdateRemotePlugins" \
+ -c "call dein#save_state()" \
+ -c "qall"
 [ -d "${HOME}/.SpaceVim/bundle/vimproc.vim" ] && make -C ~/.SpaceVim/bundle/vimproc.vim ;
